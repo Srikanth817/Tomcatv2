705ad51912f3f6655abab7ce180f8e1b69520b26
Bobita817 <srikanth817@gmail.com>
2020-02-26 20:51:33 -0700

    Acceleration and rotation data extraction

--- src/imu/imutest.cpp
+++ src/imu/imutest.cpp
@@ -1,3 +1,11 @@
+/*
+* ASEN 4028 Senior Projects -- TOMCAT
+* Script for interfacing with an Xsens MTI-3 IMU.
+* Requires XsensIMU.h/cpp to work
+* Author: Srikanth Venkataraman
+*
+*/
+
 #include <iostream>
 #include "XsensIMU.h"
 #include <unistd.h>
@@ -10,17 +18,15 @@ using namespace tomcat;
 
 
 int main() {
-
+	//Initialize new IMU object
 	XsensIMU imu(2,0x6b);
 
-
-
-	//imu.write(0x04);
-
-	unsigned char* status_buffer ;//= new unsigned char[4];
-
+	//Buffer to store data from I2C bus
+	unsigned char* status_buffer ;
+	//Get the status of the notification and measurement pipes
 	status_buffer = imu.readRegisters((uint8_t)4,0x04);
 	
+	//Extract size of message waiting in each pipe
 	uint16_t notificationSize;
 	uint16_t measurementSize;
 
@@ -29,55 +35,38 @@ int main() {
 	cout<<"Notification size: "<<hex<<notificationSize<<"\n";
 	cout<<"Measurement size: "<<hex<<measurementSize<<"\n";
 
-	//imu.write(0x06);
-	
+	//Buffer to store measurements
 	uint8_t* measurements;
-	
+
+	//Read from mmeasurement pipe
 	measurements = imu.readRegisters(measurementSize,0x06);
 	
+	//Print for debugging
 	for(int i = 0; i < measurementSize; ++i){
 			cout<<i<<": "<<hex<<(int)(measurements[i])<<"\n";
 		}
 
-	float fangle1;
-	uint8_t angle1[4];
-	angle1[0] = measurements[20];
-	angle1[1] = measurements[19];
-	angle1[2] = measurements[18];
-	angle1[3] = measurements[17];
-	memcpy(&fangle1,&angle1,4);
-
-	float fangle2;
-	uint8_t angle2[4];
-	angle2[0] = measurements[24];
-	angle2[1] = measurements[23];
-	angle2[2] = measurements[22];
-	angle2[3] = measurements[21];
-	memcpy(&fangle2,&angle2,4);
-
-	float fangle3;
-	uint8_t angle3[4];
-	angle3[0] = measurements[28];
-	angle3[1] = measurements[27];
-	angle3[2] = measurements[26];
-	angle3[3] = measurements[25];
-	memcpy(&fangle3,&angle3,4);
+	//Extract acceleration and rotation data
+	float motionData[6];
+	int indexes[6] = {23,27,31,38,42,46};
+	uint8_t tempMeas[4];
+	for (int i = 0;i<6;i++){
+		
+			cout<<indexes[i]<<"\n";
+			tempMeas[0] = measurements[indexes[i]];
+			tempMeas[1] = measurements[indexes[i]-1];
+			tempMeas[2] = measurements[indexes[i]-2];
+			tempMeas[3] = measurements[indexes[i]-3];
+			memcpy(&motionData[i],&tempMeas,4);
 
 
-	cout<<"\n";
-	cout<<"Angle 1: "<<fangle1<<" Degrees"<<"\n";
-	cout<<"Angle 2: "<<fangle2<<" Degrees"<<"\n";
-	cout<<"Angle 3: "<<fangle3<<" Degrees"<<"\n";
-
-
-	// imu.write(0xFA);
-	// imu.write(0xFF);
-	// imu.write(0x00);
-	// imu.write(0x00);
-	// imu.write(0x01);
-
-	
-	
+	}
+	 cout<<"Acc 1: "<<motionData[0]<<" m/s^2"<<"\n";
+	 cout<<"Acc 2: "<<motionData[1]<<" m/s^2"<<"\n";
+	 cout<<"Acc 3: "<<motionData[2]<<" m/s^2"<<"\n";
+	 cout<<"Rot 1: "<<motionData[3]<<" m/s"<<"\n";
+	 cout<<"Rot 2: "<<motionData[4]<<" m/s"<<"\n";
+	 cout<<"Rot 3: "<<motionData[5]<<" m/s"<<"\n";
 
 
 
