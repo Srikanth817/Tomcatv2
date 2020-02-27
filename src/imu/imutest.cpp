e15ff21da2605e4bdbdb9478f2ff7e7057c37d14
Bobita817 <srikanth817@gmail.com>
2020-02-26 21:05:28 -0700

    Added time parsing

--- src/imu/imutest.cpp
+++ src/imu/imutest.cpp
@@ -32,8 +32,8 @@ int main() {
 
 	notificationSize = (uint16_t)status_buffer[0] | ((uint16_t)status_buffer[1]<<8);
 	measurementSize = (uint16_t)status_buffer[2] | ((uint16_t)status_buffer[3]<<8);
-	cout<<"Notification size: "<<hex<<notificationSize<<"\n";
-	cout<<"Measurement size: "<<hex<<measurementSize<<"\n";
+	// cout<<"Notification size: "<<hex<<notificationSize<<"\n";
+	// cout<<"Measurement size: "<<hex<<measurementSize<<"\n";
 
 	//Buffer to store measurements
 	uint8_t* measurements;
@@ -42,9 +42,9 @@ int main() {
 	measurements = imu.readRegisters(measurementSize,0x06);
 	
 	//Print for debugging
-	for(int i = 0; i < measurementSize; ++i){
-			cout<<i<<": "<<hex<<(int)(measurements[i])<<"\n";
-		}
+	// for(int i = 0; i < measurementSize; ++i){
+	// 		cout<<i<<": "<<hex<<(int)(measurements[i])<<"\n";
+	// 	}
 
 	//Extract acceleration and rotation data
 	float motionData[6];
@@ -52,7 +52,6 @@ int main() {
 	uint8_t tempMeas[4];
 	for (int i = 0;i<6;i++){
 		
-			cout<<indexes[i]<<"\n";
 			tempMeas[0] = measurements[indexes[i]];
 			tempMeas[1] = measurements[indexes[i]-1];
 			tempMeas[2] = measurements[indexes[i]-2];
@@ -61,6 +60,29 @@ int main() {
 
 
 	}
+	uint8_t tempNano[4];
+	uint32_t nanoTime;
+
+	tempNano[0] = measurements[8];
+	tempNano[1] = measurements[7];
+	tempNano[2] = measurements[6];
+	tempNano[3] = measurements[5];
+	memcpy(&nanoTime,&tempNano,4);
+
+	uint8_t tempYear[2];
+	uint32_t year;
+	tempYear[0] = measurements[10];
+	tempYear[1] = measurements[9];
+	memcpy(&year,&tempYear,2);
+
+	uint16_t month = measurements[11];
+	uint16_t day = measurements[12];
+	uint16_t hour = measurements[13];
+	uint16_t min = measurements[14];
+	uint16_t sec = measurements[15];
+
+
+	 cout<<"Time: "<<year<<"/"<<month<<"/"<<day<<" : "<<hour<<":"<<min<<":"<<sec<<"."<<nanoTime<<"\n";
 	 cout<<"Acc 1: "<<motionData[0]<<" m/s^2"<<"\n";
 	 cout<<"Acc 2: "<<motionData[1]<<" m/s^2"<<"\n";
 	 cout<<"Acc 3: "<<motionData[2]<<" m/s^2"<<"\n";
