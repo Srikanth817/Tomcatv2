bba47fb3ef8b426233ae7880ccb883a881386ebd
Bobita817 <srikanth817@gmail.com>
2020-03-02 18:25:21 -0700

    Switched from polling to interrupts. Added UDP streaming to IMU data

--- src/imu/XsensIMU.cpp
+++ src/imu/XsensIMU.cpp
@@ -1,5 +1,13 @@
+/*
+* ASEN 4028 Senior Projects -- TOMCAT
+* I2C Read/Write functions for beaglebone black.
+* Requires XsensIMU.h to work
+* Author: Srikanth Venkataraman
+*
+*/
 #include"XsensIMU.h"
 #include<iostream>
+#include <fstream>
 #include<sstream>
 #include<fcntl.h>
 #include<stdio.h>
@@ -8,7 +16,12 @@
 #include<sys/ioctl.h>
 #include<linux/i2c.h>
 #include<linux/i2c-dev.h>
+#include <cstdint>
+#include <cstring>
+#include<string>
 using namespace std;
+
+
 
 
 namespace tomcat {
@@ -53,7 +66,7 @@ int XsensIMU::write(unsigned char value){
 unsigned char* XsensIMU::read(unsigned int number){
     unsigned char* data = new unsigned char[number];
     if(::read(this->file, data, number)!=(int)number){
-       perror("IC2: Failed to read in the full buffer.\n");
+       perror("I2C: Failed to read in the full buffer.\n");
      return NULL;
     }
   return data;
@@ -64,10 +77,80 @@ unsigned char* XsensIMU::readRegisters(unsigned int number, unsigned int fromAdd
 	this->write(fromAddress);
 	unsigned char* data = new unsigned char[number];
     if(::read(this->file, data, number)!=(int)number){
-       perror("IC2: Failed to read in the full buffer.\n");
+       perror("I2C: Failed to read in the full buffer.\n");
 	   return NULL;
     }
 	return data;
+}
+
+float * XsensIMU::getMotionData(unsigned char* measurements ){
+  float *motionData = new float[6];
+  int indexes[6] = {23,27,31,38,42,46};
+  uint8_t tempMeas[4];
+  for (int i = 0;i<6;i++){
+    
+     tempMeas[0] = measurements[indexes[i]];
+     tempMeas[1] = measurements[indexes[i]-1];
+     tempMeas[2] = measurements[indexes[i]-2];
+     tempMeas[3] = measurements[indexes[i]-3];
+     memcpy(&motionData[i],&tempMeas,4);
+
+
+  }
+  return motionData;
+}
+float  XsensIMU::getTemperature(unsigned char* measurements ){
+  float temperature ;
+  uint8_t tempTemp[4];
+  tempTemp[0] = measurements[53];
+  tempTemp[1] = measurements[52];
+  tempTemp[2] = measurements[51];
+  tempTemp[3] = measurements[50];
+  memcpy(&temperature,&tempTemp,4);
+
+  return temperature;
+}
+
+message  XsensIMU::getTime(unsigned char* measurements ){
+  uint8_t tempNano[4];
+  uint32_t nanoTime;
+
+  tempNano[0] = measurements[8];
+  tempNano[1] = measurements[7];
+  tempNano[2] = measurements[6];
+  tempNano[3] = measurements[5];
+  memcpy(&nanoTime,&tempNano,4);
+
+  // uint8_t tempYear[2];
+  // uint16_t year;
+  // tempYear[0] = measurements[10];
+  // tempYear[1] = measurements[9];
+  // memcpy(&year,&tempYear,2);
+  //If time gets screwy, fix the bad types on these variables
+  //uint16_t month = measurements[11];
+  //uint16_t day = measurements[12];
+  uint16_t hour = measurements[13];
+  uint16_t min = measurements[14];
+  uint16_t sec = measurements[15];
+  message toSend;
+  toSend.hour = hour;
+  toSend.min = min;
+  toSend.sec = sec;
+  toSend.nano = nanoTime;
+  return toSend;
+}
+
+string XsensIMU::DRDYread(){
+   ifstream fs;
+   string filepath = "/sys/class/gpio/gpio48/value";
+   fs.open((filepath).c_str());
+   if (!fs.is_open()){
+     perror("Failed to open file. Check if pin 48 is exported ");
+    }
+   string input;
+   getline(fs,input);
+   fs.close();
+   return input;
 }
 
 
--- src/imu/imutest.cpp
+++ src/imu/imutest.cpp
@@ -7,90 +7,123 @@
 */
 
 #include <iostream>
+#include <fstream>
 #include "XsensIMU.h"
 #include <unistd.h>
 #include <pthread.h>
 #include <cstdint>
 #include <cstring>
+#include<string>
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
 
 using namespace std;
 using namespace tomcat;
-
+#define PORT 5007 
 
 int main() {
+
+	ofstream dataLog;
+	//!!!!CHANGE BEFORE FLIGHT!!!!
+	// dataLog.open("telem.txt",ios::app);
+	dataLog.open("telem.txt");
 	//Initialize new IMU object
 	XsensIMU imu(2,0x6b);
 
+	int sock = 0; 
+    struct sockaddr_in serv_addr; 
+    
+    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) 
+    { 
+        cout<<"Socket creation error \n"; 
+        return -1; 
+    } 
+   
+    serv_addr.sin_family = AF_INET; 
+    serv_addr.sin_port = htons(PORT); 
+       
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
+    { 
+        cout<<"binary conversion failed \n"; 
+        return -1; 
+    } 
+   
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+    { 
+        cout<<"connection failed \n"; 
+        return -1; 
+    } 
+    
+
 	//Buffer to store data from I2C bus
-	unsigned char* status_buffer ;
-	//Get the status of the notification and measurement pipes
-	status_buffer = imu.readRegisters((uint8_t)4,0x04);
-	
-	//Extract size of message waiting in each pipe
-	uint16_t notificationSize;
-	uint16_t measurementSize;
+	int k = 0;
+	while(k<200){
+		string drdy = imu.DRDYread();
+		string one = "1"; //I hate cstrings
+		if(drdy == one){
+			unsigned char* status_buffer ;
+			//Get the status of the notification and measurement pipes
+			status_buffer = imu.readRegisters((uint8_t)4,0x04);
+			
+			//Extract size of message waiting in each pipe
+			uint16_t notificationSize;
+			uint16_t measurementSize;
 
-	notificationSize = (uint16_t)status_buffer[0] | ((uint16_t)status_buffer[1]<<8);
-	measurementSize = (uint16_t)status_buffer[2] | ((uint16_t)status_buffer[3]<<8);
-	// cout<<"Notification size: "<<hex<<notificationSize<<"\n";
-	// cout<<"Measurement size: "<<hex<<measurementSize<<"\n";
+			notificationSize = (uint16_t)status_buffer[0] | ((uint16_t)status_buffer[1]<<8);
+			measurementSize = (uint16_t)status_buffer[2] | ((uint16_t)status_buffer[3]<<8);
+			// cout<<"Notification size: "<<hex<<notificationSize<<"\n";
+			// cout<<"Measurement size: "<<hex<<measurementSize<<"\n";
 
-	//Buffer to store measurements
-	uint8_t* measurements;
-
-	//Read from mmeasurement pipe
-	measurements = imu.readRegisters(measurementSize,0x06);
-	
-	//Print for debugging
-	// for(int i = 0; i < measurementSize; ++i){
-	// 		cout<<i<<": "<<hex<<(int)(measurements[i])<<"\n";
-	// 	}
-
-	//Extract acceleration and rotation data
-	float motionData[6];
-	int indexes[6] = {23,27,31,38,42,46};
-	uint8_t tempMeas[4];
-	for (int i = 0;i<6;i++){
-		
-			tempMeas[0] = measurements[indexes[i]];
-			tempMeas[1] = measurements[indexes[i]-1];
-			tempMeas[2] = measurements[indexes[i]-2];
-			tempMeas[3] = measurements[indexes[i]-3];
-			memcpy(&motionData[i],&tempMeas,4);
+			//Buffer to store measurements
+			uint8_t* measurements;
+			uint8_t* notifications;
 
 
-	}
-	uint8_t tempNano[4];
-	uint32_t nanoTime;
+			//Read from mmeasurement pipe
+			if(notificationSize>0){
+				notifications = imu.readRegisters(notificationSize,0x05);
+			}
+			if(measurementSize>0){
+				measurements = imu.readRegisters(measurementSize,0x06);
 
-	tempNano[0] = measurements[8];
-	tempNano[1] = measurements[7];
-	tempNano[2] = measurements[6];
-	tempNano[3] = measurements[5];
-	memcpy(&nanoTime,&tempNano,4);
+			
+				//Print for debugging
+				// for(int i = 0; i < measurementSize; ++i){
+				// 		cout<<i<<": "<<hex<<(int)(measurements[i])<<"\n";
+				// 	}
 
-	uint8_t tempYear[2];
-	uint32_t year;
-	tempYear[0] = measurements[10];
-	tempYear[1] = measurements[9];
-	memcpy(&year,&tempYear,2);
+				//Extract acceleration and rotation data
+				float *motionData; 
+				motionData = imu.getMotionData(measurements);
+				message toSend = imu.getTime(measurements);
 
-	uint16_t month = measurements[11];
-	uint16_t day = measurements[12];
-	uint16_t hour = measurements[13];
-	uint16_t min = measurements[14];
-	uint16_t sec = measurements[15];
+				float temperature = imu.getTemperature(measurements);
 
+				toSend.temperature = temperature;
+				for(int i = 0; i<6;i++){
+				toSend.threeAxisData[i] = motionData[i];
+			}
+				
+				// cout<<"Time: "<<hour<<":"<<min<<":"<<sec<<"."<<nanoTime<<"\n";
+				// cout<<"Acc x: "<<motionData[0]<<" m/s^2"<<"\n";
+				// cout<<"Acc y: "<<motionData[1]<<" m/s^2"<<"\n";
+				// cout<<"Acc z: "<<motionData[2]<<" m/s^2"<<"\n";
+				// cout<<"Rot x: "<<motionData[3]<<" m/s"<<"\n";
+				// cout<<"Rot y: "<<motionData[4]<<" m/s"<<"\n";
+				// cout<<"Rot z: "<<motionData[5]<<" m/s"<<"\n";
+				// cout<<"Temperature: "<<temperature<<" degrees C\n";
+				//write to file
+				dataLog << toSend.hour<<","<< toSend.min<<","<< toSend.sec<<","<<toSend.nano<<","<< motionData[0]<<","<< motionData[1]<<","<< motionData[2]<<","<< motionData[3]<<","<< motionData[4]<<","<< motionData[5]<<","<<temperature<<"\n";
+				send(sock , &toSend , sizeof(struct message) , 0 ); 
+				delete motionData;
+				delete measurements;
+				k++;
+			}
 
-	 cout<<"Time: "<<year<<"/"<<month<<"/"<<day<<" : "<<hour<<":"<<min<<":"<<sec<<"."<<nanoTime<<"\n";
-	 cout<<"Acc 1: "<<motionData[0]<<" m/s^2"<<"\n";
-	 cout<<"Acc 2: "<<motionData[1]<<" m/s^2"<<"\n";
-	 cout<<"Acc 3: "<<motionData[2]<<" m/s^2"<<"\n";
-	 cout<<"Rot 1: "<<motionData[3]<<" m/s"<<"\n";
-	 cout<<"Rot 2: "<<motionData[4]<<" m/s"<<"\n";
-	 cout<<"Rot 3: "<<motionData[5]<<" m/s"<<"\n";
+		 delete status_buffer;
 
-
+		}
+}
 
 	return 0;
 }
