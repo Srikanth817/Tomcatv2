14a2e23bf37f8424f834fce680fe2ec0db7a1f63
Srikanth817 <srikanth817@gmail.com>
2020-03-06 15:36:27 -0700

    Add files via upload

--- src/imu/udptest.py
+++ src/imu/udptest.py
@@ -14,19 +14,22 @@ while True:
   data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
   #print(data.hex()[0:4])
   #convert to float
+  #xyz acceleration and rotation
   print(struct.unpack('f', data[0:4]))
   print(struct.unpack('f', data[4:8]))
   print(struct.unpack('f', data[8:12]))
   print(struct.unpack('f', data[12:16]))
   print(struct.unpack('f', data[16:20]))
   print(struct.unpack('f', data[20:24]))
-
+  #temperature
   print(struct.unpack('f', data[24:28]))
   #Convert to short (2 bytes)
+  # hr min sec
   print(struct.unpack('H', data[28:30]))
   print(struct.unpack('H', data[30:32]))
   print(struct.unpack('H', data[32:34]))
   #convert to int (4 bytes)
+  #nanoseconds
   print(struct.unpack('i', data[36:40]))
   print(len(data))
   print("next pt")
