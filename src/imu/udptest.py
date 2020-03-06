ce2f06117599d0638aab79cb1aa94c11a1bf71e5
Srikanth817 <srikanth817@gmail.com>
2020-03-06 15:34:42 -0700

    Add files via upload

--- src/imu/udptest.py
+++ src/imu/udptest.py
@@ -1,4 +1,5 @@
 import socket
+import struct
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5007
@@ -11,10 +12,27 @@ sock.bind((UDP_IP, UDP_PORT))
 
 while True:
   data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-  print(data)
+  #print(data.hex()[0:4])
+  #convert to float
+  print(struct.unpack('f', data[0:4]))
+  print(struct.unpack('f', data[4:8]))
+  print(struct.unpack('f', data[8:12]))
+  print(struct.unpack('f', data[12:16]))
+  print(struct.unpack('f', data[16:20]))
+  print(struct.unpack('f', data[20:24]))
+
+  print(struct.unpack('f', data[24:28]))
+  #Convert to short (2 bytes)
+  print(struct.unpack('H', data[28:30]))
+  print(struct.unpack('H', data[30:32]))
+  print(struct.unpack('H', data[32:34]))
+  #convert to int (4 bytes)
+  print(struct.unpack('i', data[36:40]))
+  print(len(data))
+  print("next pt")
+
   # if(data=="hello"):
   #   sock2.sendto("Recieved",("127.0.0.1",5006))
   
   # if(data=='166'):
   #   sock2.send(b'hello')
-  
